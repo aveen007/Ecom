e1bdaf87b0c5dca89822236f220d4d7ed618e19d
Aveen Hussein <aveen2000hussein@gmail.com>
2022-12-08 11:29:38 +0200

    pres1

---
+++ Ecom/Models/ProductSpecVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ecom.Models
+{
+    public class ProductSpecVM
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
