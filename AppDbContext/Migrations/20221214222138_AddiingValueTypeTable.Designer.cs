f863f4ec73cd5782620b1e2c585a305b1d50fc44
RamiIssa2 <rami.issa.ncd@gmail.com>
2022-12-15 00:39:40 +0200

    Adding ValueType Table

--- AppDbContext/Migrations/20221214222138_AddiingValueTypeTable.Designer.cs
+++ AppDbContext/Migrations/20221214222138_AddiingValueTypeTable.Designer.cs
@@ -4,14 +4,16 @@ using AppDbContext.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AppDbContext.Migrations
 {
     [DbContext(typeof(Ecommerce_DBContext))]
-    partial class Ecommerce_DBContextModelSnapshot : ModelSnapshot
+    [Migration("20221214222138_AddiingValueTypeTable")]
+    partial class AddiingValueTypeTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
